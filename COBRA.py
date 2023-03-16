0e941d865f92f6d4f0b17c5316aee08eb193cda0
Farhan Khan <121663493+COBRA-404-CYBER@users.noreply.github.com>
2023-03-16 10:58:04 +0600

    Add files via upload

---
+++ COBRA.py
@@ -0,0 +1,21 @@
+#-------------------------
+# ENCRYPTED BY FARHAN KHAN 🔐
+# PLZ DECODE IT BROTHER 🤧
+# LOVE FROM COBRA 404 CYBER 💫
+#-------------------------
+import os
+import time
+import platform
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/groups/1354738058401296/?ref=share&mibextid=NSMWB")
+print("\n\033[38;5;195m[\033[38;5;46m+\033[38;5;195m]\033[38;5;46m CHECKING UPDATE 💚")
+time.sleep(2)
+os.system("git pull")
+time.sleep(1)
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print("\033[1;37m[\033[38;5;46m+\033[1;37m]\033[38;5;46m CONGRATULATION YOU DEVICE SUPPORT THIS TOOLS ")
+    time.sleep(1)
+    import green
+else:
+    print("\n\033[1;37m[\033[38;5;46m!\033[1;37m]\033[38;5;46m YOUR DEVICE NOT SUPPORT THIS TOOL :(")
